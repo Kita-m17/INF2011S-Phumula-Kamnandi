2cc111ee8b21d64d90517d33b8ec94ba83a9f68a
Nikita Martin <166871190+Kita-m17@users.noreply.github.com>
2024-09-29 09:52:38 +0200

    "Added Receptionist class to repo"

---
+++ Phumla Kamnandi project/Business/Receptionist.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phumla_Kamnandi_project.Business
+{
+    public class Receptionist : Person
+    {
+        #region Data Members
+        private string receptionistID;
+        private string loginPassword;
+        private DateTime dateEmployed;
+        private decimal salary;
+        #endregion
+
+        #region Properties
+        public string ReceptionistID {  
+            get { return receptionistID; } 
+            set { receptionistID = value; }
+        }
+
+        public string LoginPassword { 
+            get { return loginPassword; }
+            set { loginPassword = value; }
+        }
+
+        public DateTime EmploymentDate { 
+            get { return dateEmployed; }
+            set { dateEmployed = value; }
+        }
+
+        public decimal Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+        #endregion
+
+        #region Constructors
+        public Receptionist(string receptionistID, string loginPassword, DateTime dateEmployed, decimal salary, string name, string surname, string phone, string email) : base(name, surname, phone, email)
+        {
+            this.receptionistID = receptionistID;
+            this.loginPassword = loginPassword;
+            this.dateEmployed = dateEmployed;
+            this.salary = salary;
+        }
+        #endregion
+
+        #region Methods
+        public decimal Payment()
+        {
+            return salary ;
+        }
+        #endregion
+    }
+}
