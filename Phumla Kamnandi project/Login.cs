813f4f6d08e72d7ffff6ce9986e5d55003860ef4
Nikita Martin <166871190+Kita-m17@users.noreply.github.com>
2024-09-29 12:31:53 +0200

    "updated login file"

--- Phumla Kamnandi project/Login.cs
+++ Phumla Kamnandi project/Login.cs
@@ -21,5 +21,74 @@ namespace Phumla_Kamnandi_project
         {
 
         }
+
+        private void ClearAll(bool value)
+        {
+            userNameTextBox.Text = "";
+            passWordTextBox.Text = "";
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            string userName = userNameTextBox.Text;
+            string password = passWordTextBox.Text;
+
+            if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please enter both the username and password");
+                return;
+            }
+            login();//for now - place holder
+
+            /*
+            bool loginSuccess = verifyLogin(userName, password);
+
+            if (loginSuccess)
+            {
+                MessageBox.Show("Login successfull");
+                login();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password. Please try again.")
+            }
+            */
+        }
+
+        /*
+         * Method that searches the database to see if the username and password matches an existing receptionist
+         * -need to connect to the db
+        private bool verifyLogin(string userName, string password) {
+            
+        }
+        */
+
+        private void login()
+        {
+            menupage menupage = new menupage();
+            menupage.Show();
+            this.Hide();
+        }
+
+        /*
+         * Debatable
+        private void ShowAll(bool value)
+        {
+            userNameLabel.Visible = value;
+            userNameTextBox.Visible = value;
+            passWordLabel.Visible = value;
+            passWordTextBox.Visible = value;
+
+            if((userNameTextBox.Text.Equals("")) && (passWordTextBox.Text.Equals("")))
+            {
+                loginButton.Visible = !value;
+            }
+            else
+            {
+                loginButton.Visible = value;
+            }
+        }
+        */
+
     }
 }
