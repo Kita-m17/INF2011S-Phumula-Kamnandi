062f9c0eb28ed74a9fd3b587e0ff7dc0c16f9188
Nikita Martin <166871190+Kita-m17@users.noreply.github.com>
2024-10-23 12:20:23 +0200

    Add files via upload

---
+++ Phumla Kamnandi project/dbo.Hotels.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Hotels] (
+    [hotelID]        VARCHAR (50)  NOT NULL,
+    [hotelName]      VARCHAR (50)  NOT NULL,
+    [noOfRooms]      INT           DEFAULT ((5)) NOT NULL,
+    [location]       VARCHAR (100) NOT NULL,
+    [facilities]		 INT           NULL,
+    PRIMARY KEY CLUSTERED ([hotelID] ASC),
+	CHECK ([facilities] IN (0,1,2,3)),
+    CONSTRAINT [UQ_HotelName] UNIQUE NONCLUSTERED ([hotelName] ASC)
+);
+
---
+++ Phumla Kamnandi project/dbo.Table_5.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[RoomAllocation]
+(
+	[roomID] VARCHAR(50) NOT NULL , 
+    [bookingID] VARCHAR(50) NOT NULL, 
+    [roomAllocationID] NCHAR(10) NOT NULL, 
+    PRIMARY KEY ([roomAllocationID]), 
+    
+)
